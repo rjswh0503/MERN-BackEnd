f18b5a2c66ee357279c78f747200a455e78005b1
rjswh0503 <rjswh0503@yahoo.com>
2025-02-12 18:38:40 +0900

    routes

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 
 // router 미들웨어가 되어 편하게 사용 가능
 const placesRoutes = require('./routes/places-routes');
+const userRoutes = require('./routes/users-routes');
 
 
 const app = express();
 
 
-app.use(placesRoutes);
+app.use('/api/places',placesRoutes); // => /api/places/...
+
+
 
 
 app.listen(5000, (req,res) => {
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,10 +2,37 @@ const exrpess = require('express');
 
 const router = exrpess.Router();
 
-router.get('/',(req,res,next) => {
-    console.log('places에서 get요청함');
-    res.send('작동 잘됨!!');
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: '세상에서 가장 유명한 고층 빌딩 중 하나',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
+
+        },
+        address: '20 W 34th St., New York, NY 10001 미국',
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid',(req,res,next) => {
+    const placesId = req.params.pid // { pId : 'p1'}
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placesId
+    });
+   res.json({place})
 });
+
+
+router.get('/user/:uid', (req,res, next) => {
+    const userId = req.params.uid;
+    const user = DUMMY_PLACES.find(u => {
+        return u.creator === userId
+    });
+    res.json({user})
+})
 
 //places 파일에 있는 router를 내보냄
 // 오로지지 내보내기만 하므로 app.js에서 불러와야 함
