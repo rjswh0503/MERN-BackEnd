96db1117970ac5d53ec75b4c09971f35e6cb2628
rjswh0503 <rjswh0503@yahoo.com>
2025-02-12 21:42:51 +0900

    gitignore

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// router 미들웨어가 되어 편하게 사용 가능
+const placesRoutes = require('./routes/places-routes');
+const userRoutes = require('./routes/users-routes');
+
+
+const app = express();
+
+
+app.use('/api/places',placesRoutes); // => /api/places/...
+
+
+/*
+오류 처리 미들웨어 생성
+앞의 미들웨어에서 오류가 있어야만 발동하는 함수이다.
+모든 라우터에서 사용 가능
+
+*/
+app.use((error, req, res, next) => {
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({Message: error.Message || '알 수 없는 애러가 발생했습니다.!!'});
+});
+
+
+app.listen(5000, (req,res) => {
+    console.log('서버 실행중....')
+});
---
+++ routes/places-routes.js
@@ -0,0 +1,65 @@
+const exrpess = require('express');
+
+
+const HttpError = require('../models/http-error');
+
+const router = exrpess.Router();
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: '세상에서 가장 유명한 고층 빌딩 중 하나',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9856644
+
+        },
+        address: '20 W 34th St., New York, NY 10001 미국',
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid', (req,res,next) => {
+    const placesId = req.params.pid // { pId : 'p1'}
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placesId
+    });
+
+   if(!place){              // constructor(message,            errorCode);
+    throw new HttpError('해당 ID값에 대한 장소를 찾지 못했습니다.', 404);
+    
+    // thorw를 할 때는 return을 쓰지 않아도 되지만, next(error)를 사용할 경우에는 앞에 return을 꼭 사용
+   };
+    res.json({ place })
+    
+});
+
+
+/* 
+첫 번째 라우트는 /api/places 로 시작하는 모든 요청을 처리한다.
+
+
+
+id 값을 찾기 위해서는 params를 사용해야 함
+*/
+
+router.get('/user/:uid', (req,res, next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId
+    });
+    
+    if(!place){
+       return next(
+       new HttpError('해당 ID값에 대한 유저를 찾지 못했습니다.', 404)
+    );
+    };
+    res.json({place})
+})
+
+//places 파일에 있는 router를 내보냄
+// 오로지지 내보내기만 하므로 app.js에서 불러와야 함
+module.exports = router;
